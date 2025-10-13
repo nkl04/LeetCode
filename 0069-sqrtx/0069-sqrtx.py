efb7a48edc793e27ad00ccad28c4084691fa5ae3
Nguyễn Kim Long <131468660+nkl04@users.noreply.github.com>
2025-10-14 00:32:54 +0700

    Time: 6 ms (30.92%), Space: 17.7 MB (51.41%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,8 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 1: return 1
-        for i in range(0,x//2+1):
-            if i ** 2 == x or (i**2 < x and (i+1)**2 > x):
-                return i
-                
-        
+        if x == 0: return 0
+        l,r = 1,x
+        while l <= r:
+            mid = (l+r) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                r = mid -1
+            else: l = mid + 1
+        return r
+
+
+   
