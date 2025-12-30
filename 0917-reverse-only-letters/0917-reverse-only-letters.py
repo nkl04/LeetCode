1b9b03254f88b457d52a35f3183ade7072bfb1cb
Nguyễn Kim Long <131468660+nkl04@users.noreply.github.com>
2025-12-30 18:23:41 +0700

    Time: 0 ms (100%), Space: 17.2 MB (93.14%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.py
+++ 0917-reverse-only-letters/0917-reverse-only-letters.py
@@ -1,20 +1,16 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        def is_letter(c):
-            return 'a' <= c <= 'z' or 'A' <= c <= 'Z'
-
-        l,r = 0, len(s) - 1
+        left, right = 0, len(s)-1
         s = list(s)
-        while l < r:
-            if not is_letter(s[l]):
-                l += 1
-            elif not is_letter(s[r]):
-                r -= 1
-            elif is_letter(s[l]) and is_letter(s[r]):
-                s[l], s[r] = s[r], s[l]
-                l += 1
-                r -= 1
+        while left < right:
+            if s[left].isalpha() and s[right].isalpha():
+                temp = s[left]
+                s[left] = s[right]
+                s[right] = temp
+                left += 1
+                right -= 1
+            elif not s[left].isalpha():
+                left += 1
+            else:
+                right -= 1
         return "".join(s)
-
-
-        
